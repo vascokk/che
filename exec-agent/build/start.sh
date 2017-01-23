4b063fa3471901e21f54217462f0b28fb5511ca9
Aleksandr Andrienko <aandrienko@codenvy.com>
2017-01-24 01:04:46 +0200

    Fix up.

--- exec-agent/build/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-cd /home/antey/projects/che/exec-agent/target/linux_amd64
-./che-websocket-terminal -static /home/antey/projects/che/plugins/plugin-terminal-ui/build  -addr :4000
